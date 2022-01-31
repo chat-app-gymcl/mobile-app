ad977beb49076629a542b708056f9f6657f6e6f3
Vitek <vit@vakula.cz>
2022-01-31 15:13:56 +0100

    id and name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.jamesonsaunders.convertwebsitetoapp',
-  appName: 'Website to App',
+  appId: 'tk.gymcl.mobile-app',
+  appName: '2AV',
   webDir: 'www',
   bundledWebRuntime: false,
 };
